bdcd7cd1622a70700fd3346d65b31450ab62e1bb
Komodo <68278515+komodoooo@users.noreply.github.com>
2023-06-27 13:24:35 +0200

    Update idk.py

--- src/idk.py
+++ src/idk.py
@@ -1,6 +1,6 @@
 import requests,re
 t:str=input("\n token: ")
-if len(t)<70:t=f"Bearer {t}"
+if len(t)==30:t=f"Bearer {t}"
 a=requests.get("https://discord.com/api/v10/users/@me",headers={"Authorization":t}).json()
 rgx=re.split("{|'|}",str(a).replace(",","\n"))
 print("\n "+"".join([i for i in rgx]),"\n")
